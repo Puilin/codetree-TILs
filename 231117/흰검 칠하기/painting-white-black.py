19a76a6806b726b4dc071ff8256111c73d4ad146
Yunseok Son <65332747+Puilin@users.noreply.github.com>
2023-11-17 04:16:04 +0900

    Title: 흰검 칠하기, Time: 175ms, Memory: 30MB, Status: Wrong Answer - CodeTree

--- 231117/흰검 칠하기/painting-white-black.py
+++ 231117/흰검 칠하기/painting-white-black.py
@@ -1,18 +1,22 @@
 n = int(input())
 
 arr = ['N' for _ in range(200001)]
+change_count = [0 for _ in range(200001)]
 
 def print_tile(cursor, direction):
     now = arr[cursor]
+    changed_num = change_count[cursor]
     if direction == 'L':
-        if now == 'N':
+        if changed_num <= 2:
             arr[cursor] = 'W'
-        elif now == 'B':
+            change_count[cursor] += 1
+        else:
             arr[cursor] = 'G'
     else: # 'R'
-        if now == 'N':
+        if changed_num <= 2:
             arr[cursor] = 'B'
-        elif now == 'W':
+            change_count[cursor] += 1
+        else:
             arr[cursor] = 'G'
 
 cursor = 100000
@@ -25,8 +29,13 @@ for _ in range(n):
             cursor -= 1
         else:
             cursor += 1
+    # 제자리에 멈춰야함
+    if direction == 'L':
+        cursor += 1
+    else:
+        cursor -= 1
 
-# ['N', 'N', 'N', 'N', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'N', 'N', 'N', ...]
+
 w = arr.count('W')
 b = arr.count('B')
 g = arr.count('G')
