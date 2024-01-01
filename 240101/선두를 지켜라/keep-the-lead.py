26ca383b07b57f964c18710cfbbb71517e13485f
Yunseok Son <65332747+Puilin@users.noreply.github.com>
2024-01-01 18:13:23 +0900

    Title: 선두를 지켜라, Time: 181ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240101/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+
+a = [0]
+b = [0]
+
+for _ in range(n):
+    v, t = map(int, input().split())
+
+    for _ in range(t):
+        pos = a[-1] + v
+        a.append(pos)
+
+for _ in range(m):
+    v, t = map(int, input().split())
+
+    for _ in range(t):
+        pos = b[-1] + v
+        b.append(pos)
+
+count = 0
+for i in range(1, len(a)):
+    if (a[i-1] >= b[i-1] and a[i] < b[i]) or (a[i-1] <= b[i-1] and a[i] > b[i]):
+        count += 1
+print(count-1) # 출발점이 같기 때문에 무조건 처음에 카운트됨
