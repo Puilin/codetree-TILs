2a972f04d75b03d4e3c61e73b2366022d6420eba
Yunseok Son <65332747+Puilin@users.noreply.github.com>
2024-01-02 22:20:57 +0900

    Title: 좌우로 움직이는 로봇, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240102/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -0,0 +1,34 @@
+n, m = map(int, input().split())
+
+arr_a = [0]
+arr_b = [0]
+for _ in range(n):
+    t, d = input().split()
+
+    for _ in range(int(t)):
+        if d == 'L':
+            pos = arr_a[-1] - 1
+            arr_a.append(pos)
+        elif d == 'R':
+            pos = arr_a[-1] + 1
+            arr_a.append(pos)
+
+for _ in range(m):
+    t, d = input().split()
+
+    for _ in range(int(t)):
+        if d == 'L':
+            pos = arr_b[-1] - 1
+            arr_b.append(pos)
+        elif d == 'R':
+            pos = arr_b[-1] + 1
+            arr_b.append(pos)
+
+count = 0
+for i in range(1, max(len(arr_a), len(arr_b))):
+    try:
+        if (arr_a[i-1] != arr_b[i-1]) and (arr_a[i] == arr_b[i]):
+            count += 1
+    except IndexError:
+        break
+print(count+1)
