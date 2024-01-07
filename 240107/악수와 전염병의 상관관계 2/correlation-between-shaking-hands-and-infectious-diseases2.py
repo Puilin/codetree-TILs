9e3897eeaa7d48882c63cc81888dd464404d8501
Yunseok Son <65332747+Puilin@users.noreply.github.com>
2024-01-07 19:31:47 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 121ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240107/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240107/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,10 +1,17 @@
 n, k, p, T = map(int, input().split())
 
 arr = [[0 for _ in range(251)] for _ in range(n+1)]
-shake_count = 0
+
+logs = []
 for _ in range(T):
     t, x, y = map(int, input().split())
+    logs.append((t, x, y))
 
+logs.sort(key=lambda x: x[0])
+
+shake_count = 0
+for log in logs:
+    t, x, y = log
     # 최초 감염자이면서 악수를 k번보다 적게 했다면
     if (x == p or y == p) and shake_count < k:
         arr[x][t] = 1
@@ -23,7 +30,5 @@ for developer in arr:
     else:
         result.append(0)
 
-del(result[-1])
-
-for i in result:
-    print(i, end="")
+for i in range(1, len(result)):
+    print(result[i], end="")
