0ed0d5fe23f6920d8974101d1bfb6604b1a37d44
Yunseok Son <65332747+Puilin@users.noreply.github.com>
2024-01-08 20:55:08 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 177ms, Memory: 25MB, Status: Passed - CodeTree

--- 240108/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240108/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -14,11 +14,7 @@ logs.sort(key=lambda x: x[0])
 
 for log in logs:
     t, x, y = log
-    # 최초 감염자
-    # if x == p and arr[x].count(1) < k+1:
-    #     arr[y][t] = 1
-    # elif y == p and arr[y].count(1) < k+1:
-    #     arr[x][t] = 1
+
     # 둘중 하나가 감염자이면서 악수를 k번보다 적게 했다면
     if (0 < arr[x].count(1) < k+1 or 0 < arr[y].count(1) < k+1):
         arr[x][t] = 1
