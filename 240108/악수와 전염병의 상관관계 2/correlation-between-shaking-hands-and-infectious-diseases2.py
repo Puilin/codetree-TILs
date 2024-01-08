23bb13e442ff06d7befed35c5e800434fb93e7e0
Yunseok Son <65332747+Puilin@users.noreply.github.com>
2024-01-08 20:54:37 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 240108/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240108/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,14 +1,7 @@
 n, k, p, T = map(int, input().split())
 
 arr = [[0 for _ in range(251)] for _ in range(n+1)]
-# [
-#   1  [1, 1, 1, 1, 0, 0, 0, 0, ...],
-#   2  [0, 1, 1, 0, 0, 0, 0, 0, ...],
-#   3  [0, 0, 0, 0, 0, 0, 0, 0, ...],
-#   4  [0, 0, 0, 0, 0, 0, 0, 0, ...],
-#   5  [0, 0, 0, 0, 0, 0, 0, 0, ...],
-#     ...
-# ]
+
 #최초 감염자
 arr[p][0] = 1
 
@@ -22,10 +15,10 @@ logs.sort(key=lambda x: x[0])
 for log in logs:
     t, x, y = log
     # 최초 감염자
-    if x == p and arr[x].count(1) < k+1:
-        arr[y][t] = 1
-    elif y == p and arr[y].count(1) < k+1:
-        arr[x][t] = 1
+    # if x == p and arr[x].count(1) < k+1:
+    #     arr[y][t] = 1
+    # elif y == p and arr[y].count(1) < k+1:
+    #     arr[x][t] = 1
     # 둘중 하나가 감염자이면서 악수를 k번보다 적게 했다면
     if (0 < arr[x].count(1) < k+1 or 0 < arr[y].count(1) < k+1):
         arr[x][t] = 1
