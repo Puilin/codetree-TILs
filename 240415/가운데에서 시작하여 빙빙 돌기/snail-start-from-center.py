9445c6543e13eb36b95cbef709789100bc381baf
Yunseok Son <65332747+Puilin@users.noreply.github.com>
2024-04-15 18:56:40 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 170ms, Memory: 25MB, Status: Passed - CodeTree

--- 240415/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ 240415/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -8,26 +8,34 @@ arr = [
 dx = [0, -1, 0, 1]
 dy = [1, 0, -1, 0]
 
-def is_range(x, y):
+def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
+
+limit = [] # 현재 단계에서 갈 수 있는 최대 칸 수
+for i in range(1, n):
+    limit.append(i)
+    limit.append(i)
+limit.append(n)
 
 dir_num = 0
 x, y = n//2, n//2
 arr[x][y] = 1
-count = 1
-
+count = 1 # 누적 숫자
+now = 0 # now < limit
 while count < n*n:
-    nx = x + dx[dir_num]
-    ny = y + dy[dir_num]
+    for l in limit:
+        for i in range(l):
+            nx = x + dx[dir_num]
+            ny = y + dy[dir_num]
 
-    if not is_range(nx, ny) or arr[nx][ny] != 0:
+            if not in_range(nx, ny):
+                break
+            
+            arr[nx][ny] = arr[x][y] + 1
+            count += 1
+            x = nx
+            y = ny
         dir_num = (dir_num + 1) % 4
-        continue
-    
-    arr[nx][ny] = arr[x][y] + 1
-    count += 1
-    x = nx
-    y = ny
 
 for row in arr:
     for num in row:
