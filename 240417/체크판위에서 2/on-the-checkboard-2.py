60e943da63654008e2fe3fd584c21cdd4c3d2f66
Yunseok Son <65332747+Puilin@users.noreply.github.com>
2024-04-17 17:35:53 +0900

    Title: 체크판위에서 2, Time: 163ms, Memory: 24MB, Status: Passed - CodeTree

--- 240417/체크판위에서 2/on-the-checkboard-2.py
+++ 240417/체크판위에서 2/on-the-checkboard-2.py
@@ -4,12 +4,17 @@ grid = []
 for _ in range(r):
     grid.append(input().split())
 
-cnt = 0
-for i in range(1, r-1):
-    for j in range(1, c-1):
-        if grid[i][j] != grid[0][0]:
-            for k in range(i+1, r-1):
-                for l in range(j+1, c-1):
-                    if grid[k][l] == grid[0][0]:
-                        cnt += 1
-print(cnt)
+def solve():
+    if grid[0][0] == grid[r-1][c-1]:
+        return 0
+    cnt = 0
+    for i in range(1, r-1):
+        for j in range(1, c-1):
+            if grid[i][j] != grid[0][0]:
+                for k in range(i+1, r-1):
+                    for l in range(j+1, c-1):
+                        if grid[k][l] == grid[0][0]:
+                            cnt += 1
+    return cnt
+
+print(solve())
