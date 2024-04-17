fc6a06202dea73cb930f405db44ad3f2c6a43206
Yunseok Son <65332747+Puilin@users.noreply.github.com>
2024-04-17 17:06:28 +0900

    Title: 최고의 13위치, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240417/최고의 13위치/best-place-of-13.py
+++ 240417/최고의 13위치/best-place-of-13.py
@@ -5,6 +5,8 @@ for _ in range(n):
     grid.append(list(map(int, input().split())))
 
 largest = 0
-for row in grid:
-    largest = max(largest, sum(row))
+for i in range(0, n):
+    for j in range(0, n-2):
+        coins = grid[i][j] + grid[i][j+1] + grid[i][j+2]
+        largest = max(largest, coins)
 print(largest)
