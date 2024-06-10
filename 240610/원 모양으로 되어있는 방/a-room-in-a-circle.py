112650598e18150f5ee4267549ad9335141cc103
Yunseok Son <65332747+Puilin@users.noreply.github.com>
2024-06-10 16:44:58 +0900

    Title: 원 모양으로 되어있는 방, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240610/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -0,0 +1,22 @@
+import sys
+
+n = int(input())
+
+rooms = []
+for _ in range(n):
+    limit = int(input())
+    rooms.append(limit)
+
+for i in range(n):
+    min_dist = sys.maxsize # 현재 최소 거리
+    dist = 0 # 이 방에서 시작해서 거리 총합 누적
+    length = 0 # 출발점에서 현재 방까지의 거리
+    for j in range(n):
+        idx = (i+j)%n
+        dist += rooms[idx] * length
+
+        length += 1
+    
+    min_dist = min(min_dist, dist)
+
+print(min_dist)
